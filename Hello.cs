213add3dc79c0196fa024bd90b73b9244c05a98d
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2024-06-01 10:34:17 +0600

    Create Hello.cs

---
+++ Hello.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           Console.WriteLine("Hello World");
+            Console.WriteLine("Hello Mim");
+            Console.WriteLine("Hello Urbi");
+            Console.ReadLine(); //After pressing enter key the program exits
+        }
+    }
+}
