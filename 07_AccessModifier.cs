5a5789dd091524cbd0557373fbbcefa5474fede8
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2024-09-19 09:17:41 +0600

    Create 07_AccessModifier.cs

---
+++ 07_AccessModifier.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods // Method = Group of related statements
+// We can organize our code by using Method 
+{
+    class Calculator{
+        int num1;
+        int num2;
+        int result;
+        void Sum(){
+            result=num1+num2;
+        }
+        void Subtract(){
+            result=num1-num2;
+        }
+        void
+    }
+    
+}
