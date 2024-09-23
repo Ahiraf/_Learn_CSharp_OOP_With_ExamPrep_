eb7765f4bbdcb0d221162f685d8d289606226a18
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2024-09-23 09:52:49 +0600

    Create 13_Greater&LessThanOverload.cs

---
+++ 13_Greater&LessThanOverload.cs
@@ -0,0 +1,52 @@
+using System;
+
+public class ThreeD
+{
+    public int x,y,z;
+    public ThreeD(int i,int j, int k)
+    {
+        x=i;
+        y=j;
+        z=k;
+    }
+    public static bool operator >(ThreeD obj1,ThreeD obj2)
+    {
+        if(Math.Sqrt(obj1.x*obj1.x + obj1.y*obj1.y + obj1.z*obj1.z)>(Math.Sqrt(obj2.x*obj2.x + obj2.y*obj2.y + obj2.z*obj2.z))){
+            return true;
+        }
+        else return false;
+    }
+    public static bool operator <(ThreeD obj1,ThreeD obj2)
+    {
+        if(Math.Sqrt(obj1.x*obj1.x + obj1.y*obj1.y + obj1.z*obj1.z)<(Math.Sqrt(obj2.x*obj2.x + obj2.y*obj2.y + obj2.z*obj2.z))){
+            return true;
+        }
+        else return false;
+    }
+    public void show()
+    {
+        Console.WriteLine(x+ "," +y +"," +z);
+    }
+}
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {  
+        int x=int.Parse(Console.ReadLine());
+        int y=int.Parse(Console.ReadLine());
+        int z=int.Parse(Console.ReadLine());
+        ThreeD obj=new ThreeD(x,y,z); // as I make the constructor with parameters, I should call it with arguments.
+        Console.Write("Here is , obj: ");
+        obj.show();
+        
+        int x2=int.Parse(Console.ReadLine());
+        int y2=int.Parse(Console.ReadLine());
+        int z2=int.Parse(Console.ReadLine());
+        ThreeD obj2=new ThreeD(x2,y2,z2); 
+        if(obj>obj2)Console.WriteLine("Point A is further than B");
+        else{
+            Console.WriteLine("Point B is further than A");
+        }
+    }
+}
