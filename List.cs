3f22bcbe2bab58886ec6521ffa7a477e88420dd9
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2024-10-15 19:11:19 +0600

    Create List.cs

---
+++ List.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+//"List" is same as "Array" used to store multiple values in a single variable .
+//Differences between List and Array : 
+//i) "Array" is List of items where one knows before hand how many 
+// elements are gonna be in list. But in case "List", one does not know before hand how many elements are gonna be in list.
+//ii) "Array" is faster but "List" is slower.
+
+
+namespace List
+{
+    public class Program 
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
