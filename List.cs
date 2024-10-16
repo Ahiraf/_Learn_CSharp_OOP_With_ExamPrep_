6defc95f19c0073098538b8caa61e3fa39122774
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2024-10-16 16:08:56 +0600

    Update List.cs

--- List.cs
+++ List.cs
@@ -12,13 +12,32 @@ using System.Threading.Tasks;
 //ii) "Array" is faster but "List" is slower.
 
 
+//Syntax : List <Datatype> name of list = new list <Datatype> ();
+//To use list I have to use the "using System.Collections.Generic" namespace
+//How to store elements in a list : by using Add() Method
+
+// The length of a List can be determined by using "ListName.Count()" function
+
+
 namespace List
 {
     public class Program 
     {
         static void Main(string[] args)
         {
-           
+            List<string>cars=new List<string>();
+            cars.Add("Maruti");
+            cars.Add("Ford");
+            cars.Add("BMW");
+            cars.Add("Ferrari");
+           // for(int i=0;i<cars.Count;i++){
+           //     Console.WriteLine(cars[i]);
+          //  }
+          //By foreach Loop : 
+          foreach(string car in cars){
+              Console.WriteLine(car);
+          }
+          
         }
     }
 }
