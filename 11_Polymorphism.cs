b58d9116c358e798c4a9242e67a0b5b70516dd67
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2024-10-22 00:41:56 +0600

    Create 11_Polymorphism.cs

---
+++ 11_Polymorphism.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+//Poly means 'Many'.
+//Morphism means 'Forms/Behaviors'.
+//Polymorphism means having many forms/behaviors.
+//'+' operator is used for Addition of two integer. For exmaple : 10 + 20 = 30 
+// It is also used for joining two Strings . For example : "Spider" + "Man" = Spiderman.
+// It can be said polymorphism of '+' operator.
+//So,if we use '+' operator between two int value it will add them up but if we use it between two strings it will concatenate them.
+// Two types polymorphism in C# , 
+//  i) Compile time polymorphism : Static Polymorphism , and we can achieve it by Method of Overloading 
+//  ii) Run time Polymorphism : Dynamic Polymorphism , and we can achieve it by Method of Overriding .
+
