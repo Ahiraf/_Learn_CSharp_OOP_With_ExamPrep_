ca0f5deabbd121f71dccf861d3e0492fd71a8486
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2024-10-29 09:58:05 +0600

    Update PassingReferencesToMethods.cs

--- PassingReferencesToMethods.cs
+++ PassingReferencesToMethods.cs
@@ -56,3 +56,72 @@ namespace PssingReferencesToMethods
       }  
   }
 }
+
+
+//If I did not use "public" Access modifier before alpha and beta variables in the "Myclass" class then I have to write a "show function" in that class
+// to print the values of alpha and beta , like following :
+
+/*
+using System;
+
+public class Myclass
+{
+    public int alpha;
+    public int beta;
+    public  Myclass(int i,int j)
+    {
+        alpha = i;
+        beta = j;
+    }
+    public bool SameAs(Myclass ob)
+    {
+        if(ob.alpha == alpha && ob.beta == beta)
+        {
+            return true;
+        }
+        return false;
+    }
+    public void Copy(Myclass ob)
+    {
+        ob.alpha = alpha;
+        ob.beta = beta;
+    }
+    public void show()
+    {
+       Console.WriteLine("alpha : "+alpha+"    beta : "+beta);
+    }    
+}
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Myclass obj1 = new Myclass(4,5);
+        Myclass obj2 = new Myclass(9,10);
+        obj1.show();
+        obj2.show();
+        Console.WriteLine();
+        
+        if(obj1.SameAs(obj2))
+        {
+            Console.WriteLine("Obj1 and Obj2 are same");
+        }
+        else
+        {
+            Console.WriteLine("Obj1 and Obj2 are not same");
+        }
+        obj1.Copy(obj2);
+        Console.WriteLine("Obj2 after copy :");
+        obj2.show();
+        if(obj1.SameAs(obj2))
+          {
+              Console.WriteLine("Obj1 and Obj2 are same");
+          }
+        else
+        {
+              Console.WriteLine("Obj1 and Obj2 are not same");
+        }
+    }
+    
+}
+*/
