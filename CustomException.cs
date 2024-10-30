c901a6297d2a83236bccc87862807eee01dbb9a4
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2024-10-30 20:41:05 +0600

    Create CustomException.cs

---
+++ CustomException.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+       int age = int.Parse(Console.ReadLine());
+       void InvalidAgeException(int i)
+       {
+           if(i<18)
+           {
+               throw new ArgumentException("this age in not valid.");
+           }
+       }
+       try
+       {
+           InvalidAgeException(age);
+       }
+       catch(ArgumentException ex)
+       {
+           Console.WriteLine("Error : "+ex.Message);
+           
+       }
+    }
+}
