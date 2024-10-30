eb65738073b59bf8028929219533d9e867a88b88
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2024-10-30 20:33:39 +0600

    Create IndexOutOfRangeException.cs

---
+++ IndexOutOfRangeException.cs
@@ -0,0 +1,35 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = {1,3,2,5};
+      try
+      {
+          int index = int.Parse(Console.ReadLine());
+           int value = arr[index];
+              Console.WriteLine("Value at index "+index+" is "+arr[index] );
+              
+          void InvalidIndexException(int i)
+          {
+              if(i<0)
+              {
+                  throw new ArgumentException("The index cannot be negative.");
+              }
+          }
+          try
+          {
+              InvalidIndexException(index);
+          }
+          catch(ArgumentException ex)
+          {
+              Console.WriteLine("Error : "+ex.Message);
+          }
+      }
+      catch(IndexOutOfRangeException ex)
+      {
+          Console.WriteLine("The index is out of range "+ex.Message);
+      }
+    }
+}
